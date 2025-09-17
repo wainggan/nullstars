37284e08d79ead465e86c02c0959d1e2935d5a28
parchii <wainggan@gmail.com>
2025-09-17 13:08:05 -0700

    fix bash

--- build-level.sh
+++ build-level.sh
@@ -1,3 +1,11 @@
-mkdir -p src/datafiles/world
-./tools/plevel/target/plevel map src/datafiles/world
-read -p "enter to continue"
+#!/usr/bin/env bash
+
+PLEVEL=./tools/plevel/target/plevel
+if [[ -f "$PLEVEL" ]]; then
+	mkdir -p src/datafiles/world
+	./tools/plevel/target/plevel map src/datafiles/world
+	read -p "enter to continue"
+else
+	echo "file $PLEVEL missing"
+fi
+
--- build-plevel.sh
+++ build-plevel.sh
@@ -1,5 +1,9 @@
+#!/usr/bin/env bash
+
 cd ./tools/plevel
 set RUST_BACKTRACE=1
 cargo build
 cp target/debug/plevel target
 cd ../..
+
+
